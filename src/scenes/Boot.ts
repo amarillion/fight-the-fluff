2d7fd6983ad7360232ffdec368d9febd0d4b9d14
Martijn van Iersel <mvaniersel@gmail.com>
2022-12-31 10:02:51 -0600

    Tower image

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -13,9 +13,9 @@ export default class extends Phaser.Scene {
 		this.load.spritesheet('fluff-spritesheet', './assets/images/Fluff_front1.png', { frameWidth: 24 });
 		this.load.spritesheet('crosshair-spritesheet', './assets/images/crosshair.png', { frameWidth: 12 });
 		this.load.spritesheet('laser-spritesheet', './assets/images/laser.png', { frameWidth: 64, frameHeight: 450 });
-		this.load.image('startgate', './assets/images/BananaBox.png');
+		this.load.image('startgate', './assets/images/Ship.png');
 		this.load.image('cloud', './assets/images/cloud.png');
-		this.load.image('endgate', './assets/images/Ship.png');
+		this.load.image('endgate', './assets/images/tower.png');
 		this.load.image('projectile', './assets/images/projectile.png');
 		this.load.audio('music', ['./assets/music/march_of_the_fluff.mp3']);
 
